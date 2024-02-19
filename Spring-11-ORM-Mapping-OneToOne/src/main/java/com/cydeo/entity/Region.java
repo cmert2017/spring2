21c96db367eca431a0a2cb654eaa7d0bcba04cf2
cmert2017 <cihanmert2075@gmail.com>
2024-02-19 17:46:35 -0500

    regions

---
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Region.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "regions")
+public class Region extends BaseEntity{
+
+    private String region;
+    private String country;
+
+
+    @OneToOne(mappedBy = "region")
+    private Employee employee;
+
+    public Region(String region, String country) {
+        this.region = region;
+        this.country = country;
+    }
+}
