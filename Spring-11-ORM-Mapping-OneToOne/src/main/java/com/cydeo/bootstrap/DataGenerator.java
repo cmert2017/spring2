32692c3b552150d080bb664562ce80e3a0498e1f
cmert2017 <cihanmert2075@gmail.com>
2024-02-19 18:11:31 -0500

    ORM3-updates

--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -2,6 +2,7 @@ package com.cydeo.bootstrap;
 
 import com.cydeo.entity.Department;
 import com.cydeo.entity.Employee;
+import com.cydeo.entity.Region;
 import com.cydeo.enums.Gender;
 import com.cydeo.repository.DepartmentRepository;
 import com.cydeo.repository.EmployeeRepository;
@@ -17,11 +18,11 @@ import java.util.List;
 public class DataGenerator implements CommandLineRunner {
 
     EmployeeRepository employeeRepository;
-    DepartmentRepository departmentRepository;
+//    DepartmentRepository departmentRepository;
 
-    public DataGenerator(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository) {
+
+    public DataGenerator(EmployeeRepository employeeRepository) {
         this.employeeRepository = employeeRepository;
-        this.departmentRepository = departmentRepository;
     }
 
     @Override
@@ -30,26 +31,43 @@ public class DataGenerator implements CommandLineRunner {
         List<Employee> employeeList = new ArrayList<>();
         List<Department> departmentList = new ArrayList<>();
 
-        Employee e1 = new Employee("Berie", "Manues", "bmanues@gmail.com", LocalDate.of(2016, 3, 11), 200000, Gender.Male);
-        Employee e2 = new Employee("XMart", "Zeng", "Xzeng@gmail.com", LocalDate.of(2013, 5, 21), 210000, Gender.Female);
-        Employee e3 = new Employee("Tartum", "Manues", "Tmanues@gmail.com", LocalDate.of(2011, 7, 1), 230000, Gender.Male);
-        Employee e4 = new Employee("Klasium", "Bari", "Kbarie@gmail.com", LocalDate.of(2015, 12, 4), 250000, Gender.Female);
-        Employee e5 = new Employee("Derium", "Wear", "dwear@gmail.com", LocalDate.of(2015, 05, 7), 240000, Gender.Female);
+        Employee e1 = new Employee("Berrie", "Manueau", "bmanueau0@dion.ne.jp", LocalDate.of(2006,04,20),154864, Gender.Female);
+        Employee e2 = new Employee("Aeriell", "McNee", "amcnee1@google.es", LocalDate.of(2009,01,26),56752, Gender.Male);
+        Employee e3 = new Employee("Sydney", "Symonds", "ssymonds2@hhs.gov", LocalDate.of(2010,05,17),95313, Gender.Female);
+        Employee e4 = new Employee("Avrom", "Rowantree", null, LocalDate.of(2014,03,02) ,119764,Gender.Female);
+        Employee e5 = new Employee("Feliks", "Morffew", "fmorffew4@a8.net", LocalDate.of(2003,01,14), 55307,Gender.Male);
 
-        Department d1 = new Department("Sports", "Outdoors");
-        Department d2 = new Department("Tools", "Hardware");
-        Department d3 = new Department("Clothing", "Home");
-        Department d4 = new Department("Phones", "Electronics");
-        Department d5 = new Department("Computer", "Electronics");
+        Department d1 = new Department("Sports","Outdoors");
+        Department d2 = new Department("Tools","Hardware");
+        Department d3 = new Department("Clothing","Home");
+        Department d4 = new Department("Phones & Tablets","Electronics");
+        Department d5 = new Department("Computers","Electronics");
+
+        Region r1 = new Region("Southwest","United States");
+        Region r2 = new Region("Central","United States");
+        Region r3 = new Region("Northwest","United States");
+        Region r4 = new Region("Quebec'","Canada");
+        Region r5 = new Region("Central","Asia");
+
+        e1.setDepartment(d1);
+        e2.setDepartment(d2);
+        e3.setDepartment(d3);
+        e4.setDepartment(d4);
+        e5.setDepartment(d5);
+
+        e1.setRegion(r1);
+        e2.setRegion(r2);
+        e3.setRegion(r3);
+        e4.setRegion(r4);
+        e5.setRegion(r5);
+
 
         employeeList.addAll(Arrays.asList(e1,e2,e3,e4,e5));
         departmentList.addAll(Arrays.asList(d1,d2,d3,d4,d5));
 
         employeeRepository.saveAll(employeeList);
-        departmentRepository.saveAll(departmentList);
+//        departmentRepository.saveAll(departmentList);
+
 
     }
-
-
-
 }
--- Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
+++ Spring-11-ORM-Mapping-OneToOne/src/main/java/com/cydeo/entity/Employee.java
@@ -3,35 +3,34 @@ package com.cydeo.entity;
 import com.cydeo.enums.Gender;
 import jakarta.persistence.*;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
+
 
 import java.time.LocalDate;
 
-@EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "employees")
 @Data
 @NoArgsConstructor
-public class Employee extends BaseEntity{
+public class Employee extends BaseEntity {
 
-    private  String firstName;
-    private  String lastName;
-    private  String email;
-
-    @Column(columnDefinition = "Date")
+    private String firstName;
+    private String lastName;
+    private String email;
+    @Column(columnDefinition = "DATE")
     private LocalDate hireDate;
-
     private int salary;
-
-
-    @Enumerated(value = EnumType.STRING)
+    @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    @OneToOne
+    @OneToOne(cascade = CascadeType.ALL)
+//    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.REMOVE})
     @JoinColumn(name = "department_id")
     private Department department;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "region_id")
+    private Region region;
 
     public Employee(String firstName, String lastName, String email, LocalDate hireDate, int salary, Gender gender) {
         this.firstName = firstName;
